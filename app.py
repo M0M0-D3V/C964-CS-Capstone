3fddee225830212aef85b03aa0bd9eb43a5173c2
Monica Hong <momo.star@gmail.com>
2024-06-18 16:36:21 -0700

    got post to work and fit model for predict to show result on success page

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import re
 import nltk
 import numpy as np
 import pandas as pd
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, session, url_for
 
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
@@ -14,7 +14,13 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.util import ngrams
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+app.secret_key = 'secret'
+
+with open('model.pkl', 'rb') as m:
+  model = pickle.load(m)
+
+with open ('vectorizer.pkl', 'rb') as v:
+  vectorizer = pickle.load(v)
 
 @app.route("/")
 def hello():
@@ -46,77 +52,70 @@ def predict():
     
     # preprocess data the same as trained model
     job_posting = preprocess_text(concat_posting)
-    print("**************jobposting******************")
-    print(job_posting)
-    
-    # fraudulent?
-    fraudulent = ''
-    
     # apply tokens
     job_posting_tokens = word_tokenize(job_posting)
-    print("**************tokens******************")
-    print(job_posting_tokens)
-    
     # apply posting sentences
     job_posting_sentences = sent_tokenize(job_posting)
-    print("**************sentences******************")
-    print(job_posting_sentences)
-    
     # apply pos tagged
     job_posting_pos_tagged = [[pos_tagging(sentence) for sentence in x] for x in job_posting_sentences]
-    
     # apply ngrams
     n = 2
     job_posting_ngrams = [generate_ngrams(x, n) for x in job_posting_tokens]
-    print("****************ngrams****************")
-    print(job_posting_ngrams)
     
-    df = pd.DataFrame(columns=['job_posting', 'fraudulent', 'job_posting_tokens', 'job_posting_sentences', 'job_posting_pos_tagged', 'job_posting_ngrams'])
+    # Flatten the lists and convert all elements to strings
+    flat_tokens = ' '.join([str(item) for sublist in job_posting_tokens for item in sublist])
+    flat_sentences = ' '.join([str(item) for sublist in job_posting_sentences for item in sublist])
+    flat_pos_tagged = ' '.join([str(item) for sublist in job_posting_pos_tagged for item in sublist])
+    flat_ngrams = ' '.join([str(item) for sublist in job_posting_ngrams for item in sublist])
+
+    # Now join everything together
+    all_text = ' '.join([job_posting, flat_tokens, flat_sentences, flat_pos_tagged, flat_ngrams])
     
-    new_data = {'job_posting': [job_posting], 'fraudulent': [fraudulent], 'job_posting_tokens': [job_posting_tokens], 'job_posting_sentences': [job_posting_sentences], 'job_posting_pos_tagged': [job_posting_pos_tagged], 'job_posting_ngrams': [job_posting_ngrams]}
+    # Transfor the new data using the same vectorizer object
+    X_new_vec = vectorizer.transform([all_text])
+    print("***************X_new_vec************************")
+    print(X_new_vec)
     
-    new_data_df = pd.DataFrame(new_data, index=[0])
-    # Convert the dictionary to a DataFrame
-    df = pd.concat([df, new_data_df], ignore_index=True)
+    prediction = model.predict(X_new_vec)
+    result = prediction[0]
+    print("****************************************************************")
+    print(result)
+    # change type to int
+    result = result.astype(int)
     
-    print("****************df****************")
-    print(df)
+    if result == 0:
+      result = 'Fake'
+    elif result == 1:
+      result = 'Real'
     
-    # make prediction
-    prediction = model.predict(df)
-    output = prediction[0]
-    print(output)
-    return redirect(url_for('success'), value=output)
+    # Store the result into session
+    session['result'] = result
+    
+    return redirect(url_for('success'))
   
-  # Render the result in a new template
-  # return str(output)
   return render_template('index.html')
 
-def success(value):
-  print(value)
-  return render_template('result.html', value=value)
+@app.route('/success')
+def success():
+  # Get the result from session
+  result = session.get('result', 'No result')
+  return render_template('result.html', result=result)
 
 def preprocess_text(text):
   # convert to lowercasae
   text = text.lower()
-  
   # Remove all URLs
   text = re.sub(r"http\S+|www\S+|https\S+", "", text, flags=re.MULTILINE)
-  
   # Remove special characters
   text = re.sub(r"[^a-zA-Z0-9\s]", "", text)
-  
   # Remove punctuation
   text = re.sub(r'[^\w\s]', '', text)
-  
   # Remove digits
   text = re.sub(r'\d', '', text)
-  
   # Remove stop words
   stop_words = set(stopwords.words('english'))
   words = [word for word in text.split() if word.lower() not in stop_words]
   text = ' '.join(words)
-  
   return text
 
 def pos_tagging(sentence):
