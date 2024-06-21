686802379fbff543a18fe4c5637788d53fafcd7a
Monica Hong <momo.star@gmail.com>
2024-06-21 01:08:45 -0700

    fix and make pretty

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.8-slim-buster
+FROM python:3.12.0-slim
 
 WORKDIR /app
 
@@ -8,6 +8,8 @@ RUN pip install --upgrade pip
 
 RUN pip install --no-cache-dir -r requirements.txt
 
+RUN python -m nltk.downloader stopwords
+
 EXPOSE 80
 
-CMD ["gunicorn", "-b", "0.0.0.0:5000", "app:app"]
+CMD ["hypercorn", "app:app", "--bind", "0.0.0.0:5000"]
--- app.py
+++ app.py
@@ -1,11 +1,13 @@
+import asyncio
 import os
-import pickle
 import re
 
+import httpx
+import joblib
 import nltk
 import numpy as np
 import pandas as pd
-from flask import (Flask, jsonify, redirect, render_template, request, session,
+from quart import (Quart, jsonify, redirect, render_template, request, session,
                    url_for)
 
 nltk.download('punkt')
@@ -15,81 +17,45 @@ from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.util import ngrams
 
-app = Flask(__name__)
+app = Quart(__name__)
 app.secret_key = 'secret'
 
-with open('model.pkl', 'rb') as m:
-  model = pickle.load(m)
-
-with open ('vectorizer.pkl', 'rb') as v:
-  vectorizer = pickle.load(v)
-
 @app.route("/")
-def hello():
-    return render_template('index.html')
+async def hello():
+    return await render_template('index.html')
   
 @app.route('/api', methods=['POST'])
-def predict():
+async def predict():
   if request.method == 'POST':
     # Get data from form inputs
+    form = await request.form
     data = []
-    data.append(request.form.get('title'))
-    data.append(request.form.get('location'))
-    data.append(request.form.get('department'))
-    data.append(request.form.get('salary'))
-    data.append(request.form.get('company-profile'))
-    data.append(request.form.get('company'))
-    data.append(request.form.get('description'))
-    data.append(request.form.get('requirements'))
-    data.append(request.form.get('benefits'))
-    data.append(request.form.get('telecommute'))
-    data.append(request.form.get('has_logo'))
-    data.append(request.form.get('has_questions'))
-    data.append(request.form.get('employment_type'))
-    data.append(request.form.get('required_experience'))
-    data.append(request.form.get('required_education'))
-    data.append(request.form.get('industry'))
+    data.append(form.get('title'))
+    data.append(form.get('location'))
+    data.append(form.get('department'))
+    data.append(form.get('salary'))
+    data.append(form.get('company-profile'))
+    data.append(form.get('company'))
+    data.append(form.get('description'))
+    data.append(form.get('requirements'))
+    data.append(form.get('benefits'))
+    data.append(form.get('telecommute'))
+    data.append(form.get('has_logo'))
+    data.append(form.get('has_questions'))
+    data.append(form.get('employment_type'))
+    data.append(form.get('required_experience'))
+    data.append(form.get('required_education'))
+    data.append(form.get('industry'))
     
     concat_posting = ' '.join(str(i) for i in data)
     
-    # preprocess data the same as trained model
-    job_posting = preprocess_text(concat_posting)
-    # apply tokens
-    job_posting_tokens = word_tokenize(job_posting)
-    # apply posting sentences
-    job_posting_sentences = sent_tokenize(job_posting)
-    # apply pos tagged
-    job_posting_pos_tagged = [[pos_tagging(sentence) for sentence in x] for x in job_posting_sentences]
-    # apply ngrams
-    n = 2
-    job_posting_ngrams = [generate_ngrams(x, n) for x in job_posting_tokens]
-    
-    # Flatten the lists and convert all elements to strings
-    flat_tokens = ' '.join([str(item) for sublist in job_posting_tokens for item in sublist])
-    flat_sentences = ' '.join([str(item) for sublist in job_posting_sentences for item in sublist])
-    flat_pos_tagged = ' '.join([str(item) for sublist in job_posting_pos_tagged for item in sublist])
-    flat_ngrams = ' '.join([str(item) for sublist in job_posting_ngrams for item in sublist])
-
-    # Now join everything together
-    all_text = ' '.join([job_posting, flat_tokens, flat_sentences, flat_pos_tagged, flat_ngrams])
-    print("***************all_text************************")
-    print(all_text)
-    
-    # Transfor the new data using the same vectorizer object
-    X_new_vec = vectorizer.transform([all_text])
-    print("***************X_new_vec************************")
-    print(X_new_vec)
-    
-    prediction = model.predict(X_new_vec)
-    result = prediction[0]
+    result = await preprocess_and_predict(concat_posting)
     print("****************************************************************")
     print(result)
-    # change type to int
-    result = result.astype(int)
     
     if result == 0:
       result = 'Fake'
-    else:
+    elif result == 1:
       result = 'Real'
     
     # Store the result into session
@@ -97,13 +63,49 @@ def predict():
     
     return redirect(url_for('success'))
   
-  return render_template('index.html')
+  return await render_template('index.html')
 
 @app.route('/success')
-def success():
+async def success():
   # Get the result from session
   result = session.get('result', 'No result')
-  return render_template('result.html', result=result)
+  return await render_template('result.html', result=result)
+
+async def preprocess_and_predict(data):
+  model = joblib.load('model.pkl')
+  vectorizer = joblib.load('vectorizer.pkl')
+  # preprocess data the same as trained model
+  job_posting = preprocess_text(data)
+  # apply tokens
+  job_posting_tokens = word_tokenize(job_posting)
+  # apply posting sentences
+  job_posting_sentences = sent_tokenize(job_posting)
+  # apply pos tagged
+  job_posting_pos_tagged = [[pos_tagging(sentence) for sentence in x] for x in job_posting_sentences]
+  # apply ngrams
+  n = 2
+  job_posting_ngrams = [generate_ngrams(x, n) for x in job_posting_tokens]
+  
+  # Flatten the lists and convert all elements to strings
+  flat_tokens = ' '.join([str(item) for sublist in job_posting_tokens for item in sublist])
+  flat_sentences = ' '.join([str(item) for sublist in job_posting_sentences for item in sublist])
+  flat_pos_tagged = ' '.join([str(item) for sublist in job_posting_pos_tagged for item in sublist])
+  flat_ngrams = ' '.join([str(item) for sublist in job_posting_ngrams for item in sublist])
+
+  # Now join everything together
+  all_text = ' '.join([job_posting, flat_tokens, flat_sentences, flat_pos_tagged, flat_ngrams])
+  print("***************all_text************************")
+  print(all_text)
+  
+  # Transfor the new data using the same vectorizer object
+  X_new_vec = vectorizer.transform([all_text])
+  print("***************X_new_vec************************")
+  print(X_new_vec)
+  
+  prediction = model.predict(X_new_vec)
+  prediction = prediction[0]
+  prediction = prediction.astype(int)
+  return prediction
 
 def preprocess_text(text):
   # convert to lowercasae
